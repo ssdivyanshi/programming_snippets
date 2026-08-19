f64673e1552573772cd598ac309a90213293decf
ssdivyanshi <sailaja.divyanshi@gmail.com>
2026-08-19 20:24:12 +0530

    Create factorial.js

---
+++ JavaScript/factorial.js
@@ -0,0 +1,10 @@
+// Program to find the factorial of a number
+
+let num = 5;
+let factorial = 1;
+
+for (let i = 1; i <= num; i++) {
+    factorial *= i;
+}
+
+console.log("Factorial =", factorial);
